61ef0dd3a5fd9a14dac791d61e0a51eabada2eac
Md Aatif Tegginmani <mdaatif84@gmail.com>
2023-07-12 17:02:10 +0530

    1

--- components/TopicList.jsx
+++ components/TopicList.jsx
@@ -5,7 +5,7 @@ import { HiPencilAlt } from "react-icons/hi";
 
 const getTopics = async () => {
   try {
-     const res = await fetch("http://localhost:3000/api/topics", {
+     const res = await fetch(process.env.NEXT_PUBLIC_API_URL, {
       cache: "no-store",
     });
     if (!res.ok) {
